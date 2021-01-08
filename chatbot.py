18fcc79f6da1b52ac423ec9bc2ee92e422157bb7
Siddharth-coder13 <sid021099kumar@gmail.com>
2021-01-08 22:45:44 +0530

    Added skelton code in chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+# import chatterbot library for the chat bot
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# make a set of conversations(Chirag)
+
+
+
+
+
+
+# Train the bot with the training data(Siddharth)
+
+
+
+
+
+# get response from the bot (siddharth)
+
