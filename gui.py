37f1990bea42eda21f9589b4fa24e3a711803947
Siddharth-coder13 <sid021099kumar@gmail.com>
2021-01-09 15:41:02 +0530

    Added some codes in gui.py

--- gui.py
+++ gui.py
@@ -19,7 +19,6 @@ send_image = PhotoImage(file="send.png")
 send = Button(window, image= send_image, height=100, width =200, command = send_msg, borderwidth=0)
 send.place(x=200, y= 400)
 
-
 # use chatbot functions to display the responses in GUI
 
 
