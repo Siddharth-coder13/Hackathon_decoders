c70a9005d87125d40419861565f81340548d17bc
chirag0282 <chirag_b@bt.iitr.ac.in>
2021-01-09 16:27:00 +0530

    Added questions in chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,31 +4,37 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # make a set of conversations(Chirag)
+
+#greetings
 convo1 = ["Pardon",
           "Hello", "Hi, Its a pleasure to meet you!",
-          "How can you help me?", " I can be your guide for Roorkee! :)"
-          "All right!, tell me something about IIT Roorkee","Okay, What do you want to know?"]
+          "How can you help me?", " I can be your guide for Roorkee! :)",
+          "All right!, tell me something about IIT Roorkee","Okay, What do you want to know?"
+          "How are you doing?","I am doing great!"]
 
 
 convo2 = ["Which is the boys hostel for fresher?","Rajendra Bhawan",
           "Which is the girls hostel for fresher?", "Kasturba",
           "Which place is most favoured for photography?","Main Building",
           "Which Bhawan has the best canteen?","Cautley Bhawan",
-          "Name of cafe in IITR?","Georgia"]
+          "Name of cafe in IITR?","Georgia",
+          ""]
 
 #historyofiitr
 convo3 = ["When was it established?","It was officially established in 1847.",
+          "Established by whom?","It was established by Sir James Thomason",
           "When did it recieved IIT status?","It was given IIT status in 2001."
           "What were the initial names of IITR?","College of Civil Engineering,Thomason College of Civil Engineering,University of Roorkee",
           ""
           ]
 
-#transportation
+#transportation&accommodation
 convo4 = ["How do I get to Roorkee?","Well, the nearest airhead to Roorkee is Dehradun, around 70km away, but find Delhi a more convenient airport. Delhi is about 180km away",
           "How can I get a bus to roorkee?","Buses can be obtained either from I.S.B.T. Kashmere Gate, Delhi or from I.S.B.T. Anand Vihar Ghaziabad",
           "Which trains go to Roorkee?","Shatabdi and the JanShatabdi trains go all the way to Dehradun, passing through Roorkee",
-          ""]
-
+          "Accomodation for students?","Hostels will be made available",
+          "Accomodation for parents?","Very limited accommodation is available in IIT Roorkee guest houses for the parents.",
+          ""
 #undefined
 convo5 = ["Name of banks are inside the campus?","Punjab National Bank and State Bank of India",
           "How is the climate?","You'll see a continental, read extreme, climate. This means that it starts getting cold in about November.",
