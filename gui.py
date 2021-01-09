23b25857f776ae3619d2533468df46e2390d475b
Siddharth-coder13 <sid021099kumar@gmail.com>
2021-01-09 12:54:47 +0530

    Added some codes in gui.py

--- gui.py
+++ gui.py
@@ -4,16 +4,23 @@ import chatbot
 
 # Make a default window using tkinter
 
-
-
-
-
+window = Tk()
+window.title("Chatbot")
+window.geometry("400x500")
 
 # Add widgets in the window
 
+#write commands to send message
+def send_msg() :
+    print("message")
 
+send = Button(text="Send", command = send_msg)
+send.pack()
 
 
 
 
 # use chatbot functions to display the responses in GUI
+
+
+window.mainloop()
